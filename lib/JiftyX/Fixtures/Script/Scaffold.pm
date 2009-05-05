95b4ab282a3b60a8ed3b28e42e4e1920c02e1a72
shelling <navyblueshellingford@gmail.com>
2009-05-05 17:00:15 +0800

    scaffold half-completed

--- lib/JiftyX/Fixtures/Script/Scaffold.pm
+++ lib/JiftyX/Fixtures/Script/Scaffold.pm
@@ -4,6 +4,13 @@ package JiftyX::Fixtures::Script::Scaffold;
 use warnings;
 use strict;
 
+use Jifty;
+use Jifty::Everything;
+
+use File::Spec;
+use File::Basename;
+use YAML qw(Dump LoadFile);
+
 use base qw(
   JiftyX::Fixtures::Script::Base
   App::CLI::Command
@@ -19,6 +26,24 @@ sub options {
 sub run {
   my ($self, $args) = @_;
   print "INFO - run " . ref($self) ."\n";
+  Jifty->new;
+
+  my @models = map { basename($_) } glob(
+    File::Spec->catfile(
+      $self->{config}->{app_root},
+      "lib",
+      $self->{config}->{framework}->{ApplicationClass},
+      "Model",
+      "*"
+    )
+  );
+
+  for my $model (@models) {
+    $model =~ s/\.pm//g;
+    my %columns =  map { $_->name() => "" } Jifty->app_class("Model",$model)->columns;
+    print Dump [\%columns];
+  }
+
 }
 
 1;
